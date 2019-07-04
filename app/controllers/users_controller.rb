dcc9d3740e25264283f6343704800e5dc41534a1
kamayatsu <50704071+kamayatsu@users.noreply.github.com>
2019-07-05 02:18:34 +0900

    Create users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:show]
+  def index
+    user = User.find(current_user.id)
+    @sleeps = user.sleeps.order('date DESC').includes(:user).limit(7).reverse
+    @sleep = user.sleeps.last
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @sleeps = @user.sleeps.order('date DESC').includes(:user).limit(14).reverse
+  end
+
+  private
+  def correct_user
+    path = request.fullpath.split('/')[2].to_i
+    if current_user.id != path
+      redirect_to root_path
+    end
+  end
 end
