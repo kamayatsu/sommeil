90df2b3c5d01cae541332ef3432b9998e0c9ca47
kamayatsu <50704071+kamayatsu@users.noreply.github.com>
2019-07-07 15:08:32 +0900

    Update sleep.rb

--- app/models/sleep.rb
+++ app/models/sleep.rb
@@ -2,11 +2,11 @@ class Sleep < ApplicationRecord
   belongs_to :user
   validates :slept_time, presence: true
   validates :wakeup_time, presence: true
-  validate :dates_cannnot_be_registered_if_there_is_no_yesterdays_date
+  validate :dates_cannnot_be_registered_if_there_is_no_yesterdays_date, on: [:new, :create]
 
   def dates_cannnot_be_registered_if_there_is_no_yesterdays_date
     return if date.present? && user.sleeps.blank?
-    if date != Sleep.last.date.tomorrow
+    if date != user.sleeps.last.date.tomorrow
       errors.add(:date, "You can't register that there is no data about before days.")
     end
   end
